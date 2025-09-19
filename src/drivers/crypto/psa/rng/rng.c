9ebf9427cfbf2224eb895f3a778e5a4955a2d5db
mbedsyst <zalman15320011351@gmail.com>
2025-09-19 15:10:28 +0530

    Defined the RNG Crypto Interface with PSA Frontend

--- src/drivers/crypto/psa/rng/rng.c
+++ src/drivers/crypto/psa/rng/rng.c
@@ -1,9 +1,25 @@
 #include "interfaces/crypto/rng/rng.h"
 #include "crypto_status.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
+#include <psa/crypto.h>
+#include <psa/crypto_extra.h>
+
+LOG_MODULE_REGISTER(crypto_rng_interface);
+
 int crypto_rng(uint8_t *output, size_t output_len)
 {
-    (void)output;
-    (void)output_len;
-    return CRYPTO_ERR_NOT_IMPLEMENTED;
+    psa_status_t status = psa_generate_random(output, output_len);
+    if (status != PSA_SUCCESS) 
+    {
+        LOG_INF("psa_generate_random failed! (Error: %d)", status);
+        return -1;
+    }
+    
+    return 0;
 }
